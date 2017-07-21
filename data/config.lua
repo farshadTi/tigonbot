6b26a33b32b2df68e41a6d92dce746e030420714
farshadTi <mrfarshadti@gmail.com>
2017-07-21 14:17:18 +0200

    TIGON v1

---
+++ data/config.lua
@@ -0,0 +1,26 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "BanHammer",
+    "Fun",
+    "GroupManager",
+    "Msg-Checks",
+    "Plugins",
+    "Tools",
+    "Write",
+    "CleanD",
+    "test",
+    "cleanB"
+  },
+  info_text = "",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    106512277,
+    341017390
+  }
+}
+return _
+end
