b69000f3272aa80a7ba2779b75599d725e8ed78a
farshadTi <30344098+farshadTi@users.noreply.github.com>
2017-07-21 17:02:55 +0430

    Update matador.sh

--- matador.sh
+++ matador.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $HOME/MaTaDoR
+cd $HOME/tigonbot
 
 install() {
 	    cd tg
@@ -23,32 +23,7 @@ install() {
 		chmod +x tg
 }
 
-function print_logo() {
-	echo -e "\033[38;5;600m" 
-	echo -e "  __  __      _____     ____        ____  "
-	echo -e " |  \/  | __ |_   _|_ _|  _ \  ___ |  _ \ "
-	echo -e " | |\/| |/ _  || |/ _  | | | |/ _ \| |_) | "
-	echo -e " |_|  |_|\__,_||_|\__,_|____/ \___/|_| \_\ "
-	echo -e "\n\e[36m"
-}
 
-function logo_play() {
-    declare -A txtlogo
-    seconds="0.010"
-    txtlogo[1]="  __  __      _____     ____        ____ "
-    txtlogo[2]=" |  \/  | __ |_   _|_ _|  _ \  ___ |  _ \ "
-    txtlogo[3]=" | |\/| |/ _  || |/ _  | | | |/ _ \| |_) | "
-    txtlogo[4]=" |_|  |_|\__,_||_|\__,_|____/ \___/|_| \_\ "
-    printf "\033[38;5;600m\t"
-    for i in ${!txtlogo[@]}; do
-        for x in `seq 0 ${#txtlogo[$i]}`; do
-            printf "${txtlogo[$i]:$x:1}"
-            sleep $seconds
-        done
-        printf "\n\t"
-    done
-    printf "\n"
-}
 
 if [ "$1" = "install" ]; then
   install
@@ -61,16 +36,7 @@ if [ ! -f ./tg/tgcli ]; then
  fi
 
 
-   print_logo
-   echo -e "\033[38;5;208m"
-   echo -e "  __  __       _     ____  _ ____        ___ "
-   echo -e " |  \/  | __ _| |__ |  _ \(_)  _ \ ___  / _ \ "
-   echo -e " | |\/| |/ _  | '_ \| | | | | |_) / _ \| | | | "
-   echo -e " | |  | | (_| | | | | |_| | |  _ < (_) | |_| | "
-   echo -e " |_|  |_|\__,_|_| |_|____/|_|_| \_\___/ \___/ "
-   echo -e "\033[0;00m"
-   echo -e "\e[36m"
-   logo_play
+  
    #sudo service redis-server restart
    #./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
    ./tg/tgcli -s ./bot/bot.lua $@
