c2d992706bc25109edc56b863b00b2d656af4a00
farshadTi <30344098+farshadTi@users.noreply.github.com>
2017-07-21 17:15:26 +0430

    Update Tools.lua

--- plugins/Tools.lua
+++ plugins/Tools.lua
@@ -1158,7 +1158,7 @@ return chat_list(msg)
 	   tdcli.sendMessage(matches[2], 0, 1, "Group has been removed by admin command", 1, 'html')
     return '_Group_ *'..matches[2]..'* _removed_'
 		end
-if matches[1] == 'matador' or matches[1] == 'Matador' then
+if matches[1] == 'TIGONBOT' or matches[1] == 'TIGONBOT' then
 return tdcli.sendMessage(msg.to.id, msg.id, 1, _config.info_text, 1, 'md')
     end
 if matches[1] == 'adminlist' and is_admin(msg) or matches[1] == 'Adminlist' and is_admin(msg) then
@@ -1215,8 +1215,8 @@ patterns = {
 "^([Ll]eave)$",		
 "^[!/#]([Aa]utoleave) (.*)$",
 "^([Aa]utoleave) (.*)$",	
---"^[!/#]([Mm]atador)$",
--'"^([Mm]atador)$",		
+--"^[!/#]([Tt]igon)$",
+--'"^([Tt]igon)$",		
 "^[!/#]([Cc]reategroup) (.*)$",
 "^([Cc]reategroup) (.*)$",		
 "^[!/#]([Cc]reatesuper) (.*)$",
@@ -1276,4 +1276,3 @@ patterns = {
 }, 
 run = run, pre_process = pre_process
 }
--- #End By @MahDiRoO
